199497ca79fe182a1355f16a85e2165854c9d1a2
Elakkuvan Rajamani <elakkuvanbecs@gmail.com>
2022-06-21 00:02:52 +0200

    Input Form Hook

---
+++ src/hooks/use-input.js
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+const useInput = (validateValue) => {
+  const [enteredValue, setEnteredValue] = useState("");
+  const [isTouched, setIsTouched] = useState(false);
+
+  const isValueValid = validateValue(enteredValue);
+  const hasErrors = !isValueValid && isTouched;
+
+  const valueChangeHandler = (event) => {
+    setEnteredValue(event.target.value);
+  };
+
+  const valueBlurHandler = (event) => {
+    setIsTouched(true);
+  };
+
+  const reset = () => {
+    setEnteredValue('');
+    setIsTouched(false);
+  };
+
+  return {
+    value: enteredValue,
+    isValid: isValueValid,
+    hasErrors,
+    valueChangeHandler,
+    valueBlurHandler,
+    reset
+  };
+};
+
+export default useInput;
